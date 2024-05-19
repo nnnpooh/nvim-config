f279136706bb5e8facbc3ae78f8fd616935959dd
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-05-19 17:13:36 +0700

    Init

---
+++ init.lua
@@ -0,0 +1,15 @@
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+end 
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+
+    { import = 'plugins' },
+})
