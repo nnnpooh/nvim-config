bf24bfb57c755977738f8759690fbc29d0994174
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-05-21 21:43:48 +0700

    Update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -176,6 +176,9 @@ return { -- LSP Configuration & Plugins
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"stylua", -- Used to format Lua code
+			"black",
+			"prettier",
+			"prettierd",
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
