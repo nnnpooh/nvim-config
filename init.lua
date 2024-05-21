0658af0d5040b3b99bab6dfede795fdcb53c4ed3
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-05-21 13:09:50 +0700

    Update

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- Setup all my goodies.
+require 'settings'
+require 'keymaps'
+require 'commands'
 
-require("default.vim")
-
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -12,6 +13,25 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-
-	{ import = "plugins" },
+	{ import = "plugins" }
+}, {
+	ui = {
+		-- If you are using a Nerd Font: set icons to an empty table which will use the
+		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = '⌘',
+			config = '🛠',
+			event = '📅',
+			ft = '📂',
+			init = '⚙',
+			keys = '🗝',
+			plugin = '🔌',
+			runtime = '💻',
+			require = '🌙',
+			source = '📄',
+			start = '🚀',
+			task = '📌',
+			lazy = '💤 ',
+		},
+	},
 })
